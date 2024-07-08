23de7bdb1f57704e489a7e4f4de80149160afa4e
Ofish <vc.ofir@gmail.com>
2024-07-08 17:01:54 -0400

    managed to read file. Working on highlights currently broken

--- main.ts
+++ main.ts
@@ -1,23 +1,24 @@
 import { match } from "assert";
-import { Plugin } from "obsidian";
+import { Editor, Plugin } from "obsidian";
 import * as fs from "fs";
 import * as path from "path";
 
 export default class misscap extends Plugin {
 	statusBarElement: HTMLSpanElement;
+	PROPERNOUNS: Set<string>;
 
-	onload() {
+	async onload() {
 		this.statusBarElement = this.addStatusBarItem().createEl("span");
-		this.readActiveFileAndUpdateLineCount();
+		this.PROPERNOUNS = await this.loadProperNouns();
+		this.readActiveFileAndfindCapWords();
 
 		this.app.workspace.on("editor-change", (editor) => {
 			const content = editor.getDoc().getValue();
-			this.updateLineCount(content);
-			this.findCapWords(content);
+			this.findCapWords(content, editor);
 		});
 
 		this.app.workspace.on("active-leaf-change", () => {
-			this.readActiveFileAndUpdateLineCount();
+			this.readActiveFileAndfindCapWords();
 		});
 	}
 
@@ -25,14 +26,15 @@ export default class misscap extends Plugin {
 		this.statusBarElement.remove();
 	}
 
-	private async readActiveFileAndUpdateLineCount() {
+	private async readActiveFileAndfindCapWords() {
 		const file = this.app.workspace.getActiveFile();
 		if (file) {
 			const content = await this.app.vault.read(file);
-			this.updateLineCount(content);
+			if (this.app.workspace.activeEditor()) {
+				editor = this.app.workspace.activeEditor();
+			}
 			this.findCapWords(content);
 		} else {
-			this.updateLineCount(undefined);
 			this.findCapWords(undefined);
 		}
 	}
@@ -43,29 +45,46 @@ export default class misscap extends Plugin {
 		this.statusBarElement.textContent = `${count} ${linesWord}`;
 	}
 
-	private findCapWords(fileContent?: string) {
+	private findCapWords(fileContent?: string, editor: any) {
 		if (!fileContent) return;
 
-		let matches = [];
+		let matches: Array<any> = [];
 		let capitalizedWords = fileContent
 			? [...fileContent.matchAll(/\b[A-Z][a-z]*\b/g)]
 			: [];
-		console.log(path.resolve(__dirname, "properNouns.txt"));
-		console.log(fs.readFileSync("../properNouns.txt"), "utf8");
-		for (const word of capitalizedWords) {
+		for (const wordAndInfo of capitalizedWords) {
 			if (
-				fileContent[word.index - 1] === "\t" ||
-				fileContent[word.index - 2] === "." ||
-				fileContent[word.index - 1] === "!" ||
-				fileContent[word.index - 1] === "?" ||
-				fileContent[word.index - 1] === '"' ||
-				fileContent[word.index - 1] === "'" ||
-				word[0] === word[0].toUpperCase()
+				fileContent[wordAndInfo.index - 1] === "\t" ||
+				fileContent[wordAndInfo.index - 2] === "." ||
+				fileContent[wordAndInfo.index - 1] === "!" ||
+				fileContent[wordAndInfo.index - 1] === "?" ||
+				fileContent[wordAndInfo.index - 1] === '"' ||
+				fileContent[wordAndInfo.index - 1] === "'" ||
+				fileContent[wordAndInfo.index - 1] === "#" ||
+				wordAndInfo[0] === wordAndInfo[0].toUpperCase() ||
+				this.PROPERNOUNS.has(wordAndInfo[0])
 			) {
 				continue;
 			}
-			matches.push(word);
+			matches.push(wordAndInfo);
 		}
 		console.log(matches);
+		if (matches.length < 1) {
+			return;
+		}
 	}
+
+	private async loadProperNouns(): Promise<Set<string>> {
+		const filePath = path.join(
+			this.app.vault.configDir,
+			"plugins",
+			"misscap",
+			"properNouns.txt"
+		);
+		const data = await this.app.vault.adapter.read(filePath);
+		const nouns = new Set(data.split(/\r?\n/));
+		return nouns;
+	}
+
+	private;
 }
