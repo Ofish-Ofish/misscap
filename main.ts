4b4e72d2651c587f311d9a8d1f079dae72345100
Ofish <vc.ofir@gmail.com>
2024-07-12 02:04:54 -0400

    basic highlighting works

--- main.ts
+++ main.ts
@@ -1,24 +1,27 @@
-import { match } from "assert";
 import { Editor, Plugin, MarkdownView, Notice, Modal } from "obsidian";
 import * as fs from "fs";
 import * as path from "path";
 
-export default class misscap extends Plugin {
+export default class Misscap extends Plugin {
 	statusBarElement: HTMLSpanElement;
 	PROPERNOUNS: Set<string>;
+	debounceTimer: number;
 
 	async onload() {
 		this.statusBarElement = this.addStatusBarItem().createEl("span");
 		this.PROPERNOUNS = await this.loadProperNouns();
-		this.readActiveFileAndfindCapWords();
+		this.readActiveFileAndFindCapWords();
 
 		this.app.workspace.on("editor-change", (editor) => {
-			const content = editor.getDoc().getValue();
-			this.findCapWords(content, editor);
+			clearTimeout(this.debounceTimer);
+			this.debounceTimer = window.setTimeout(() => {
+				const content = editor.getDoc().getValue();
+				this.findCapWords(content, editor);
+			}, 300);
 		});
 
 		this.app.workspace.on("active-leaf-change", () => {
-			this.readActiveFileAndfindCapWords();
+			this.readActiveFileAndFindCapWords();
 		});
 	}
 
@@ -26,7 +29,7 @@ export default class misscap extends Plugin {
 		this.statusBarElement.remove();
 	}
 
-	private async readActiveFileAndfindCapWords() {
+	private async readActiveFileAndFindCapWords() {
 		const file = this.app.workspace.getActiveFile();
 		if (file) {
 			const content = await this.app.vault.read(file);
@@ -39,7 +42,7 @@ export default class misscap extends Plugin {
 		}
 	}
 
-	private findCapWords(fileContent?: string, editor: any) {
+	private async findCapWords(fileContent?: string, editor: any) {
 		if (!fileContent) return;
 
 		let matches: Array<any> = [];
@@ -47,34 +50,65 @@ export default class misscap extends Plugin {
 			? [...fileContent.matchAll(/\b[A-Z][a-z]*\b/g)]
 			: [];
 		for (const wordAndInfo of capitalizedWords) {
+			const word = wordAndInfo[0];
+			const startIndex = wordAndInfo.index;
+
+			// Check if the word is already within a <span> tag
+			const spanword = fileContent.substring(
+				Math.max(0, startIndex - 22),
+				Math.min(fileContent.length, startIndex + 7)
+			);
+			if (
+				spanword.includes('<span class="misscap">') ||
+				spanword.includes("</span>")
+			) {
+				continue;
+			}
+
 			if (
 				fileContent[wordAndInfo.index - 1] === "\t" ||
 				fileContent[wordAndInfo.index - 2] === "." ||
-				fileContent[wordAndInfo.index - 1] === "!" ||
-				fileContent[wordAndInfo.index - 1] === "?" ||
+				fileContent[wordAndInfo.index - 3] === "." ||
+				fileContent[wordAndInfo.index - 1] === "\n" ||
+				fileContent[wordAndInfo.index - 2] === "!" ||
+				fileContent[wordAndInfo.index - 3] === "!" ||
+				fileContent[wordAndInfo.index - 2] === "?" ||
+				fileContent[wordAndInfo.index - 3] === "?" ||
 				fileContent[wordAndInfo.index - 1] === '"' ||
 				fileContent[wordAndInfo.index - 1] === "'" ||
 				fileContent[wordAndInfo.index - 1] === "#" ||
+				fileContent[wordAndInfo.index - 1] === ">" ||
+				fileContent[wordAndInfo.index - 1] === "“" ||
+				fileContent[wordAndInfo.index - 1] === "‘" ||
+				fileContent[wordAndInfo.index - 1] === "’" ||
+				fileContent[wordAndInfo.index - 1] === "”" ||
+				fileContent[wordAndInfo.index - 1] === undefined ||
 				wordAndInfo[0] === wordAndInfo[0].toUpperCase() ||
 				this.PROPERNOUNS.has(wordAndInfo[0])
 			) {
 				continue;
 			}
 			matches.push(wordAndInfo);
+			console.log(fileContent[wordAndInfo.index - 1]);
 		}
-		console.log(matches);
+
 		if (matches.length < 1) {
 			return;
 		}
 
+		// Sort matches by their starting index in descending order
+		matches.sort((a, b) => b.index - a.index);
+		console.log(matches);
+		// Replace matches in the original content
+		let newContent = fileContent;
 		for (const match of matches) {
-			console.log(match[0]);
-			// const newContent = fileContent.replace(
-			// 	match[0],
-			// 	`<span class="misscap">${match[0]}</span>`
-			// );
-			// editor.getDoc().setValue(newContent);
+			newContent =
+				newContent.substring(0, match.index) +
+				`<span class="misscap">${match[0]}</span>` +
+				newContent.substring(match.index + match[0].length);
 		}
+
+		editor.getDoc().setValue(newContent);
 	}
 
 	private async loadProperNouns(): Promise<Set<string>> {
